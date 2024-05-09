de511f361f6553f413c37360112793844afd4c6c
Mario <mperdiguerobarrera@safareyes.es>
2024-05-09 16:28:07 +0200

    ✨ Create home view logic

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,16 +1,38 @@
 package controller;
 
 import javafx.application.Platform;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import utils.Utils;
 
 public class HomeController implements ControllerInterface {
+
+    public Button btnAssignments;
+
     @Override
     public Stage getStage() {
-        return null;
+        return (Stage) btnAssignments.getScene().getWindow();
     }
 
     @Override
     public void closeWindow() {
         Platform.exit();
     }
+
+    public void openCompaniesWindow() {
+        Utils.openWindow(Utils.WindowType.COMPANIES, this);
+    }
+
+    public void openStudentsWindow() {
+        Utils.openWindow(Utils.WindowType.STUDENTS, this);
+    }
+
+    public void openTutorsWindow() {
+        Utils.openWindow(Utils.WindowType.TUTORS, this);
+    }
+
+    public void openAssignmentsWindow() {
+        Utils.openWindow(Utils.WindowType.ASSIGNMENTS, this);
+    }
+
 }

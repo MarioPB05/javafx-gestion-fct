588009075477321625128b10490c69f6db876873
Mario <mperdiguerobarrera@safareyes.es>
2024-05-21 18:55:00 +0200

    ✨ Initialize companies table

--- src/main/java/controller/CompaniesController.java
+++ src/main/java/controller/CompaniesController.java
@@ -1,11 +1,11 @@
 package controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.Event;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import model.Company;
 import utils.Utils;
 
 import java.util.Arrays;
@@ -19,11 +19,63 @@ public class CompaniesController implements ControllerInterface {
     public Button btnSearch;
     public VBox modalitiesContainer;
     public VBox journeysContainer;
+    public TableView<Company> tblCompanies;
 
     public void initialize() {
         initializeButtons();
         initializeModalities();
         initializeJourneys();
+        initializeTable();
+    }
+
+    private void initializeTable() {
+        TableColumn<Company, String> colCIF = new TableColumn<>("CIF");
+        TableColumn<Company, String> colName = new TableColumn<>("Nombre");
+        TableColumn<Company, String> colAddress = new TableColumn<>("Dirección");
+        TableColumn<Company, String> colPostalCode = new TableColumn<>("C.P.");
+        TableColumn<Company, String> colCity = new TableColumn<>("Localidad");
+        TableColumn<Company, String> colJourney = new TableColumn<>("Jornada");
+        TableColumn<Company, String> colModality = new TableColumn<>("Modalidad");
+        TableColumn<Company, String> colEmail = new TableColumn<>("Email");
+
+        TableColumn<Company, String> colManagerDNI = new TableColumn<>("DNI del responsable");
+        TableColumn<Company, String> colManagerName = new TableColumn<>("Nombre del responsable");
+        TableColumn<Company, String> colManagerSurname = new TableColumn<>("Apellidos del responsable");
+
+        TableColumn<Company, String> colTutorDNI = new TableColumn<>("DNI del tutor");
+        TableColumn<Company, String> colTutorName = new TableColumn<>("Nombre del tutor");
+        TableColumn<Company, String> colTutorSurname = new TableColumn<>("Apellidos del tutor");
+        TableColumn<Company, String> colTutorPhone = new TableColumn<>("Teléfono del tutor");
+
+        // Configuramos las columnas de la empresa
+        Utils.configureColumn(colCIF, Utils.createStringProperty(Company::getCif));
+        Utils.configureColumn(colName, Utils.createStringProperty(Company::getName));
+        Utils.configureColumn(colAddress, Utils.createStringProperty(Company::getAddress));
+        Utils.configureColumn(colPostalCode, Utils.createStringProperty(Company::getPostalCode));
+        Utils.configureColumn(colEmail, Utils.createStringProperty(Company::getEmail));
+        Utils.configureColumn(colModality, Utils.createStringProperty(company -> company.getModality().getName()));
+        Utils.configureColumn(colJourney, Utils.createStringProperty(company -> company.getJourneyType().getName()));
+
+        // Configuramos las columnas del responsable
+        Utils.configureColumn(colManagerDNI, Utils.createStringProperty(company -> company.getCompanyManager().getDni()));
+        Utils.configureColumn(colManagerName, Utils.createStringProperty(company -> company.getCompanyManager().getName()));
+        Utils.configureColumn(colManagerSurname, Utils.createStringProperty(company -> company.getCompanyManager().getSurname()));
+
+        // Configuramos las columnas del tutor
+        Utils.configureColumn(colTutorDNI, Utils.createStringProperty(company -> company.getCompanyTutor().getDni()));
+        Utils.configureColumn(colTutorName, Utils.createStringProperty(company -> company.getCompanyTutor().getName()));
+        Utils.configureColumn(colTutorSurname, Utils.createStringProperty(company -> company.getCompanyTutor().getSurname()));
+        Utils.configureColumn(colTutorPhone, Utils.createStringProperty(company -> company.getCompanyTutor().getPhone()));
+
+        // Añadimos las columnas a la tabla
+        tblCompanies.getColumns().addAll(
+                colCIF, colName, colAddress, colPostalCode, colEmail, colModality, colJourney,
+                colManagerDNI, colManagerName, colManagerSurname,
+                colTutorDNI, colTutorName, colTutorSurname, colTutorPhone
+        );
+
+        ObservableList<Company> companies = Company.getAllCompanies();
+        tblCompanies.setItems(companies);
     }
 
     private void initializeButtons() {

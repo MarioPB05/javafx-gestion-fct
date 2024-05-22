435b26560773becafdb11ac0d9ebff631a07a2ce
Mario <mperdiguerobarrera@safareyes.es>
2024-05-22 18:57:00 +0200

    ✨ Create import students method

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,10 +1,19 @@
 package controller;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import model.Student;
 import utils.Utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 public class HomeController implements ControllerInterface {
 
@@ -33,6 +42,26 @@ public class HomeController implements ControllerInterface {
 
     public void openStudentsExportWindow() {
         Utils.openWindow(Utils.WindowType.STUDENT_FORM, this);
+    }
+
+    public void importStudents() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Importar Alumnos");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("DAT Files", "*.dat"));
+
+        java.io.File file = fileChooser.showOpenDialog(getStage());
+        if (file != null) {
+            try (FileInputStream fis = new FileInputStream(file);
+                 ObjectInputStream ois = new ObjectInputStream(fis)) {
+                ObservableList<Student> students = FXCollections.observableArrayList((ArrayList<Student>) ois.readObject());
+
+                students.forEach(Student::save);
+
+                Utils.showAlert("Éxito", "Alumnos importados correctamente", javafx.scene.control.Alert.AlertType.INFORMATION);
+            } catch (IOException | ClassNotFoundException e) {
+                Utils.errorLogger(e.getMessage());
+            }
+        }
     }
 
     public void openTutorsWindow() {

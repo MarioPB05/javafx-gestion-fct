406abf3e854095a6843ef52d4acb1def27374722
Mario <mperdiguerobarrera@safareyes.es>
2024-05-22 20:13:33 +0200

    ✨ Create delete method

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,11 +1,14 @@
 package model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import lombok.*;
 import utils.ConexionDB;
 import utils.Utils;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
@@ -42,6 +45,49 @@ public class Student implements Serializable {
             return rows > 0;
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public Boolean delete() {
+        try {
+            ConexionDB database = Utils.getDatabaseConnection();
+            String query = "DELETE FROM student WHERE id = ?";
+            Object[] params = {this.id};
+
+            int rows = database.ejecutarInstruccionPreparada(query, params);
+
+            return rows > 0;
+        } catch (SQLException e) {
+            Utils.errorLogger(e.getMessage());
+            return false;
+        }
+    }
+
+    public static ObservableList<Student> getStudents() {
+        ObservableList<Student> students = FXCollections.observableArrayList();
+
+        try {
+            ConexionDB database = Utils.getDatabaseConnection();
+            String query = "SELECT * FROM student";
+            database.ejecutarConsulta(query);
+            ResultSet rs = database.getResultSet();
+
+            while (rs.next()) {
+                Student student = new Student(
+                        rs.getInt("id"),
+                        rs.getString("dni"),
+                        rs.getString("name"),
+                        rs.getString("surname"),
+                        rs.getDate("birthdate").toLocalDate()
+                );
+
+                students.add(student);
+            }
+
+            return students;
+        } catch (SQLException e) {
+            Utils.errorLogger(e.getMessage());
+            return null;
         }
     }
 

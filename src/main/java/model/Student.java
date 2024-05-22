5c7fdc9251aff8bccdfbc4fefb3a71a09898cf61
Mario <mperdiguerobarrera@safareyes.es>
2024-05-22 18:55:47 +0200

    🗃️ Create save method

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,9 +1,12 @@
 package model;
 
 import lombok.*;
+import utils.ConexionDB;
+import utils.Utils;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.sql.SQLException;
 import java.time.LocalDate;
 
 @Getter
@@ -26,6 +29,20 @@ public class Student implements Serializable {
         this.name = name;
         this.surname = surname;
         this.birthdate = birthdate;
+    }
+
+    public Boolean save() {
+        try {
+            ConexionDB database = Utils.getDatabaseConnection();
+            String query = "INSERT INTO student (dni, name, surname, birthdate) VALUES (?, ?, ?, ?)";
+            Object[] params = {this.dni, this.name, this.surname, Utils.formatDateDatabase(this.birthdate)};
+
+            int rows = database.ejecutarInstruccionPreparada(query, params);
+
+            return rows > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }

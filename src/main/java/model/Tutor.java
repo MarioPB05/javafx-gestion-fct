698522633a5796003456858540d7e39f244052c6
Mario <mperdiguerobarrera@safareyes.es>
2024-05-22 21:12:56 +0200

    ✨ Create save, remove and get methods

--- src/main/java/model/Tutor.java
+++ src/main/java/model/Tutor.java
@@ -1,6 +1,13 @@
 package model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import lombok.*;
+import utils.ConexionDB;
+import utils.Utils;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Getter
 @Setter
@@ -8,10 +15,78 @@ import lombok.*;
 @AllArgsConstructor
 public class Tutor {
 
-    private int id;
+    private Integer id;
     private String name;
     private String surname;
     private String email;
     private String phone;
+
+    public Tutor(String name, String surname, String email, String phone) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Boolean save() {
+        try {
+            ConexionDB database = Utils.getDatabaseConnection();
+            String query = "INSERT INTO tutor (name, surname, email, phone) VALUES (?, ?, ?, ?)";
+            Object[] params = {this.name, this.surname, this.email, this.phone};
+
+            int rows = database.ejecutarInstruccionPreparada(query, params);
+
+            if (this.id == null) {
+                this.id = database.ultimoID("id", "tutor");
+            }
+
+            return rows > 0;
+        } catch (SQLException e) {
+            Utils.errorLogger(e.getMessage());
+            return false;
+        }
+    }
+
+    public Boolean delete() {
+        try {
+            ConexionDB database = Utils.getDatabaseConnection();
+            String query = "DELETE FROM tutor WHERE id = ?";
+            Object[] params = {this.id};
+
+            int rows = database.ejecutarInstruccionPreparada(query, params);
+
+            return rows > 0;
+        } catch (SQLException e) {
+            Utils.errorLogger(e.getMessage());
+            return false;
+        }
+    }
+
+    public static ObservableList<Tutor> getTutors() {
+        ObservableList<Tutor> tutors = FXCollections.observableArrayList();
+
+        try {
+            ConexionDB database = Utils.getDatabaseConnection();
+            String query = "SELECT * FROM tutor";
+            database.ejecutarConsulta(query);
+            ResultSet rs = database.getResultSet();
+
+            while (rs.next()) {
+                Tutor tutor = new Tutor(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getString("surname"),
+                    rs.getString("email"),
+                    rs.getString("phone")
+                );
+                tutors.add(tutor);
+            }
+
+            return tutors;
+        } catch (SQLException e) {
+            Utils.errorLogger(e.getMessage());
+            return null;
+        }
+    }
 
 }
